6cf97c50441434129c6bb2cf5add7bf392ae9ac4
anajessica24 <anajessica_lima@hotmail.com>
2025-08-26 16:00:04 -0300

    :sparkles:

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@
 # Dar permissão ao executável C
 chmod +x backend/csrc/build/btc-wallet-c
 
-# Iniciar o Flask via Gunicorn na porta 8000
-gunicorn --bind=0.0.0.0:8000 backend.pysrc.app:app --workers=4
+# Rodar Flask via Gunicorn na porta 8000
+gunicorn --bind=0.0.0.0:8000 app:app --workers=4
