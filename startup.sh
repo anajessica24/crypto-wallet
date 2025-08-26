fe96f0fc7cfb619a0a0edcef2ca64d5ffa646305
anajessica24 <anajessica_lima@hotmail.com>
2025-08-26 15:40:56 -0300

    :sparkles:

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Comando para iniciar a aplicação com Gunicorn
 gunicorn --bind=0.0.0.0 --workers=4 backend.pysrc.app:app
+chmod +x backend/csrc/build/btc-wallet-c
+gunicorn --bind=0.0.0.0:8000 app:app
